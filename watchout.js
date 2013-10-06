8692d2cb362764ef1f8033d3d6249bccc4226935
Hack Reactor Students <github@hackreactor.com>
2013-10-05 17:05:31 -0700

    Rebuilds and refactors to better utilize d3

--- watchout.js
+++ watchout.js
@@ -1,95 +1,155 @@
 var score = 0;
 var highScore = 0;
+var w = 700;
+var h = 500;
+var n = 20;
+var r = 10;
+var enemyCoord = [];
+var mouseCoord = [];
+var player;
 
-var bSettings = {
-  numEnemies: 20,
-  svgWidth: 700,
-  svgHeight: 500
+
+var svg = d3.select("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .on('mousemove',mousemove)
+;
+
+var update = function(data){
+
+  var circle = svg.selectAll('.enemy')
+    .data(data);
+
+  circle.enter().append('circle')
+    .attr('class','enemy')
+    .attr('r',r)
+    .attr('fill','white')
+    .attr('cx',function(d) { return d.x; })
+    .attr('cy',function(d) { return d.y; });
+
+  circle
+    .transition()
+    .duration(2000)
+    .attr('cx',function(d) { return d.x; })
+    .attr('cy',function(d) { return d.y; });
 };
 
-var gameBoard = d3.select(".gameBoard")
-  .append("svg")
-  .attr("width", bSettings.svgWidth)
-  .attr("height", bSettings.svgHeight);
+var createPlayer = function() {
+  player = svg.append('circle')
+  .attr('class', 'player')
+  .attr('fill', 'orange')
+  .attr('r', r)
+  .attr('cx', w/2)
+  .attr('cy', h/2)
+}();
+
+function mousemove(d,i) {
+  mouseCoord = d3.mouse(this);
+  player.attr('cx', mouseCoord[0])
+  .attr('cy', mouseCoord[1]);
+
+}
 
 
-var createPlayers = function(n) {
-  var cx, cy;
-  var r = 10;
-  var color = 'black';
+var start = function() {
+  setInterval(function(){
+    for (var i = 0; i < n; i++) {
+      enemyCoord.push(
+        {x: Math.max((Math.random()*(w-r)),r),
+         y: Math.max((Math.random()*(h-r)),r)});
+    }
+    update(enemyCoord);
+    enemyCoord = [];
+  }, 2000);
+}();
 
-  for (var i = 0; i < n; i++) {
-    cx = Math.max(Math.floor(Math.random()*(bSettings.svgWidth-r)),r);
-    cy = Math.max(Math.floor(Math.random()*(bSettings.svgHeight-r)), r);
 
-    var enemy = gameBoard.append("circle")
-      .attr("cx", cx)
-      .attr("cy", cy)
-      .attr("r", r)
-      .style("fill", color);
-  }
 
-  var range = _.range(0,bSettings.numEnemies);
-  d3.select('body').selectAll('circle').data(range);
+// function move(){
+//     var dragTarget = d3.select(this);
+//     dragTarget
+//         .attr("cx", function(){
+//           if ((d3.event.dx + parseInt(dragTarget.attr("cx"))) > 695) {
+//             return 690;
+//           } else if ((d3.event.dx + parseInt(dragTarget.attr("cx"))) < 5){
+//             return 10;
+//           } else {
+//           return (d3.event.dx + parseInt(dragTarget.attr("cx")));
+//           }
+//         })
+//         .attr("cy", function(){
+//           if ((d3.event.dy + parseInt(dragTarget.attr("cy"))) > 495) {
+//             return 490;
+//           } else if ((d3.event.dy + parseInt(dragTarget.attr("cy"))) < 5){
+//             return 10;
+//           } else {
+//           return d3.event.dy + parseInt(dragTarget.attr("cy"));
+//           }
+//         });
+// };
 
-  d3.select('body').selectAll('circle').attr('class',function(d){
-    return 'enemy ' + d;
-  });
+// createPlayers(bSettings.numEnemies);
 
-  var player = gameBoard.append("circle")
-      .attr("cx", bSettings.svgWidth/2)
-      .attr("cy", bSettings.svgHeight/2)
-      .attr("r", r)
-      .style("fill", 'blue')
-      .attr('class', 'thePlayer')
-      .call(d3.behavior.drag().on("drag", move));
-};
 
-function move(){
-    var dragTarget = d3.select(this);
-    dragTarget
-        .attr("cx", function(){
-          if ((d3.event.dx + parseInt(dragTarget.attr("cx"))) > 695) {
-            return 690;
-          } else if ((d3.event.dx + parseInt(dragTarget.attr("cx"))) < 5){
-            return 10;
-          } else {
-          return (d3.event.dx + parseInt(dragTarget.attr("cx")));
-          }
-        })
-        .attr("cy", function(){
-          if ((d3.event.dy + parseInt(dragTarget.attr("cy"))) > 495) {
-            return 490;
-          } else if ((d3.event.dy + parseInt(dragTarget.attr("cy"))) < 5){
-            return 10;
-          } else {
-          return d3.event.dy + parseInt(dragTarget.attr("cy"));
-          }
-        });
-};
+// // var detectCollisions function() {
+//   setInterval(function(){
+//      score++;
+//      d3.selectAll('.enemy').each(function() {
+//        d3.select('.scoreboard').html('High Score: ' + highScore.toString() + '</br> Current Score: ' +score.toString());
+//        if (Math.abs((d3.select(this).attr('cy') - d3.select('.thePlayer').attr('cy')) < 10)
+//          && Math.abs((d3.select(this).attr('cx') - d3.select('.thePlayer').attr('cx'))) < 10) {
+//          if (score > highScore) {
+//           highScore = score;
+//          }
+//          score = 0;
+//          d3.select('.thePlayer').attr('r', 0);
+//          setTimeout(function(){
+//          d3.select('.thePlayer').attr('r', 10);
+//          },200)
+//          }
+//      });
+//   }, 10);
+// // };
 
-createPlayers(bSettings.numEnemies);
 
-var moveEnemies = function() {
-  d3.select('body').selectAll('.enemy').each(function(){
-    d3.select(this).transition().duration(2000).attr('cx', Math.max(Math.floor(Math.random()*(bSettings.svgWidth-10)),10))
-    .attr('cy', Math.max(Math.floor(Math.random()*(bSettings.svgHeight-10)),10));
-  });
+// function startForce() {
+// alert('hello')
+// var nodes = d3.selectAll('enemy').map(function(i) {
+//   return {index: i}
+// });
 
-};
+// var links = d3.selectAll('enemy').map(function(i) {
+//   return {index: i.cy}
+// })
 
-setInterval(function(){
-   score++;
-   d3.selectAll('.enemy').each(function() {
-     d3.select('.scoreboard').html('High Score: ' + highScore.toString() + '</br> Current Score: ' +score.toString());
-     if (Math.abs((d3.select(this).attr('cy') - d3.select('.thePlayer').attr('cy')) < 10)
-       && Math.abs((d3.select(this).attr('cx') - d3.select('.thePlayer').attr('cx'))) < 10) {
-       if (score > highScore) {
-        highScore = score;
-       }
-       score = 0;
-      }
-   });
-}, 10);
 
-setInterval(moveEnemies, 2000);
+
+// var force = d3.layout.force()
+//     .nodes(nodes)
+//     .charge(-300)
+//     .on('tick', tick)
+//     .start()
+
+// function tick(e) {
+//   var k = 6 * e.alpha;
+//   nodes.forEach(function(o, i) {
+//     o.y += i & 1 ? k : -k;
+//     o.x += i & 2 ? k : -k;
+//   });
+// }
+// }
+
+
+
+// // var startForce = function() {
+// //   d3.selectAll('.enemy')
+// //   .call(force.drag)
+
+
+// // }
+
+// setInterval(function() {
+//   startForce();
+//   moveEnemies();
+
+// }, 2000);
